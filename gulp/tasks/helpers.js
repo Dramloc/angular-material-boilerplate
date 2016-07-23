391d8d28ef5dfd066b2995150bd45a1ba0857282
Antoine Colmard <a.colmard@gmail.com>
2016-07-23 17:46:51 +0200

    Add build to gulp serve task dependencies.

--- gulp/tasks/helpers.js
+++ gulp/tasks/helpers.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var config = require('../config');
 
-gulp.task('watch', ['browserify:build', 'scripts:watch', 'sass:watch', 'templates:watch', 'views:watch']);
+gulp.task('watch', ['scripts:watch', 'sass:watch', 'templates:watch', 'views:watch']);
 gulp.task('build', ['browserify:build', 'scripts:build', 'sass:build', 'templates:build', 'views:build']);
 gulp.task('lint', ['scripts:lint']);
 gulp.task('clean', ['browserify:clean', 'scripts:clean', 'sass:clean', 'templates:clean']);
 
-gulp.task('serve', config.production ? null : ['watch'], function () {
+gulp.task('serve', config.production ? ['build'] : ['build', 'watch'], function () {
     $.connect.server({
         root: config.serve.root,
         livereload: !config.production,
