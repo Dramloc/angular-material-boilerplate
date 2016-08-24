8de5f03b94724aeaabd92e0408c5b6265763b84a
Antoine Colmard <a.colmard@gmail.com>
2016-08-24 23:48:48 +0200

    Add entries to task stub

--- app/task/task.service.js
+++ app/task/task.service.js
@@ -7,18 +7,58 @@ function taskService($q, $timeout) {
     var stub = [
         {
             id: ++stubIndex,
-            name: "learn angular",
-            description: "first things first",
+            name: 'AngularJS — Superheroic JavaScript MVW Framework',
+            description: 'HTML enhanced for web apps!',
             done: true
         }, {
             id: ++stubIndex,
-            name: "learn angular material",
-            description: "with great power comes great design",
+            name: 'Angular Material',
+            description: 'For developers using AngularJS, Angular Material is both a UI Component framework and a reference implementation of Google\'s Material Design Specification. This project provides a set of reusable, well-tested, and accessible UI components based on Material Design.',
             done: true
         }, {
             id: ++stubIndex,
-            name: "build boilerplate",
-            description: "put it all together",
+            name: 'gulp.js - the streaming build system',
+            description: 'Automate and enhance your workflow',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'angular translate',
+            description: 'i18n for your Angular app, made easy!',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'Browserify',
+            description: 'Browserify lets you require(`\'modules\') in the browser by bundling up all of your dependencies.',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'Sass: Syntactically Awesome Style Sheets',
+            description: 'CSS with superpowers',
+            done: true
+        }, {
+            id: ++stubIndex,
+            name: 'ESLint - Pluggable JavaScript linter',
+            description: 'The pluggable linting utility for JavaScript and JSX',
+            done: true
+        }, {
+            id: ++stubIndex,
+            name: 'angular-ui/ui-router',
+            description: 'The de-facto solution to flexible routing with nested views in AngularJS',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'The MIT License',
+            description: 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'Material icons',
+            description: 'Material icons are beautifully crafted, delightful, and easy to use in your web, Android, and iOS projects. Learn more about material design and our process for making these icons in the system icons section of the material design guidelines.',
+            done: false
+        }, {
+            id: ++stubIndex,
+            name: 'LiveReload',
+            description: 'The Web Developer Wonderland (a happy land where browsers don\'t need a Refresh button)',
             done: false
         }
     ];
